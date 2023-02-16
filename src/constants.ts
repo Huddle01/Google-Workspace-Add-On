9c1c634058df456c882c92235591bb36c6f1e0c1
Akshit1311 <a4748g@gmail.com>
2023-02-17 03:24:34 +0530

    update: secrets

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,8 @@
 const MAX_MESSAGE_LENGTH = 40;
+
+const scopes = [
+  "https://www.googleapis.com/auth/script.external_request",
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/gmail.addons.execute",
+  "https://www.googleapis.com/auth/gmail.addons.current.message.readonly",
+];
